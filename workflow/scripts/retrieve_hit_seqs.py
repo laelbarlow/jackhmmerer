f6f9784ef9cb5cb9af9ee1c759643bd3e9f3b2a2
Lael Barlow <lael@ualberta.ca>
2021-02-09 06:25:02 -0700

    Update the retrieve_hit_seqs.py script to retrieve each full-length sequence only once, even if the ID appears in the results more than once.

--- workflow/scripts/retrieve_hit_seqs.py
+++ workflow/scripts/retrieve_hit_seqs.py
@@ -7,6 +7,7 @@ from Bio import SeqIO
 
 # Generate a list of sequence IDs for positive hits (hits included in the final
 # jackhmmer profile).
+ids_already_used = []
 with open(snakemake.input.result_file) as infh,\
 open(snakemake.output.fasta, 'a') as o:
     for seq in SeqIO.parse(infh, 'fasta'):
@@ -14,9 +15,19 @@ open(snakemake.output.fasta, 'a') as o:
         # subsequence coordinates).
         original_id = seq.id.rsplit('/', 1)[0]
 
-        # Retrieve sequences with esl-sfetch from the HMMer3 package.
-        subprocess.call(['esl-sfetch',
-                         snakemake.input.database_file,
-                         original_id
-                        ], stdout=o)
+        # Don't retrieve the same full-length sequence more than once (even
+        # though there may be more than one domain from the same sequence in
+        # the results).
+        if original_id in ids_already_used:
+            pass
+
+        else:
+            # Retrieve sequences with esl-sfetch from the HMMer3 package.
+            subprocess.call(['esl-sfetch',
+                             snakemake.input.database_file,
+                             original_id
+                            ], stdout=o)
+
+            # Add the ID to the list.
+            ids_already_used.append(original_id)
 
